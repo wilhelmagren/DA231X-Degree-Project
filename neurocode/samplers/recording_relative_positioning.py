c496edaeee795c59a40b6f320b4ca9537eba45ea
ulysses@bund-subsystem <wagren@kth.se>
2021-11-30 21:08:23 +0100

    R-RP implemenation, also tested slightly, still need to properly handle different recording lengths...

--- neurocode/samplers/recording_relative_positioning.py
+++ neurocode/samplers/recording_relative_positioning.py
@@ -7,48 +7,47 @@ Last edited: 29-11-2021
 import torch
 import numpy as np
 
-from .base import BaseSampler
+from .base import PretextSampler
 
 
-class RecordingRelativePositioningSampler(BaseSampler):
-    def __init__(self, metadata, info, tau=5, gamma=.5, **kwargs):
+class RecordingRelativePositioningSampler(PretextSampler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def _parameters(self, tau=5, gamma=.5, **kwargs):
         self.tau = tau
         self.gamma = gamma
-        super().__init__(metadata, info, **kwargs)
 
-    def _sample_pair(self, recording_idx=None):
-        if recording_idx is None:
-            recording_idx = self._sample_recording()
-
+    def _sample_pair(self):
         batch_anchors = list()
         batch_samples = list()
         batch_labels = list()
-        for _ in range(self.info['batch_size']):
-            anchor_idx = self.rng.randint(0, len(self.metadata['data'][recording_idx]))
-            same_recording = self.rng.binomial(1, self.gamma)
-            sample_idx = None
-            sample_recording_idx = recording_idx
+        reco_idx1 = self._sample_recording()
+        for _ in range(self.batch_size):
+            win_idx1 = self._sample_window(recording_idx=reco_idx1)
+            pair_type = self.rng.binomial(1, self.gamma)
+            reco_idx2, win_idx2 = -1, -1
 
-            if same_recording == 0:
-                # Sample from the same recording
-                sample_idx = self.rng.randint(max(0, anchor_idx - self.tau), min(anchor_idx + self.tau, len(self.metadata['data'][recording_idx])))
-                while sample_idx == anchor_idx:
-                    sample_idx = self.rng.randint(max(0, anchor_idx - self.tau), min(anchor_idx + self.tau, len(self.metadata['data'][recording_idx])))
-                
-            elif same_recording == 1:
-                # Sample from another recording, either previous or next
-                sample_recording_idx = recording_idx + 1
-                if sample_recording_idx >= len(self.metadata['data']):
-                    sample_recording_idx = 0
+            if pair_type == 0:
+                # sample from other recording
+                reco_idx2 = self._sample_recording()
+                while reco_idx1 == reco_idx2:
+                    reco_idx2 = self._sample_recording()
 
-                if anchor_idx >= len(self.metadata['data'][sample_recording_idx]):
-                    continue
 
-                sample_idx = self.rng.randint(max(0, anchor_idx - self.tau), min(anchor_idx + self.tau, len(self.metadata['data'][sample_recording_idx])))
-            
-            batch_anchors.append(self.metadata['data'][recording_idx][anchor_idx][None])
-            batch_samples.append(self.metadata['data'][sample_recording_idx][sample_idx][None])
-            batch_labels.append(float(same_recording))
+                win_idx2 = self._sample_window(recording_idx=reco_idx2)
+                while np.abs(win_idx1 - win_idx2) > self.tau or win_idx1 == win_idx2:
+                    win_idx2 = self._sample_window(recording_idx=reco_idx2)
+            elif pair_type == 1:
+                # sample from the same recording
+                reco_idx2 = reco_idx1
+                win_idx2 = self._sample_window(recording_idx=reco_idx2)
+                while np.abs(win_idx1 - win_idx2) > self.tau or win_idx1 == win_idx2:
+                    win_idx2 = self._sample_window(recording_idx=reco_idx2)
+
+            batch_anchors.append(self.data[reco_idx1][win_idx1][0][:2][None])
+            batch_samples.append(self.data[reco_idx2][win_idx2][0][:2][None])
+            batch_labels.append(float(pair_type))
 
         ANCHORS = torch.Tensor(np.concatenate(batch_anchors, axis=0))
         SAMPLES = torch.Tensor(np.concatenate(batch_samples, axis=0))
