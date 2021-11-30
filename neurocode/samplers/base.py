bcdef9d371ffac502536f735f7b911389120f142
ulysses@bund-subsystem <wagren@kth.se>
2021-11-30 17:49:05 +0100

    Parent sampler class PretextSampler. Ssets up information and base sampling of recordings, but each child class have to implement setting up parameters and window pair sampling accordingly

--- neurocode/samplers/base.py
+++ neurocode/samplers/base.py
@@ -1,9 +1,9 @@
 """
-implementation of BaseSampler parent class for each specific sampler,
+implementation of Pretext task sampler parent class for each specific sampler,
 built using torch Sampler. 
 
 Authors: Wilhelm Ågren <wagren@kth.se>
-Last edited: 29-11-2021
+Last edited: 30-11-2021
 """
 
 import numpy as np
@@ -11,45 +11,46 @@ import numpy as np
 from torch.utils.data.sampler import Sampler
 
 
-class BaseSampler(Sampler):
-    def __init__(self, metadata, info, **kwargs):
-        self.metadata = metadata
-        self._setup(info, **kwargs)
+class PretextSampler(Sampler):
+    def __init__(self, data, info, **kwargs):
+        self.data = data
+        self.info = info
+        self._parameters(**kwargs)
+        self._setup(**kwargs)
 
     def __len__(self):
-        return self.info['n_samples']
+        return self.n_samples
 
     def __iter__(self):
-        for i in range(self.info['n_samples']):
-            yield self.sampled[i] if self.info['presample'] else self._sample_pair()
-    
-    def _setup(self, fullinfo, n_samples=None, batch_size=None, presample=False, seed=1):
-        if n_samples is None:
-            raise ValueError('number of samples to draw not given')
+        for i in range(self.n_samples):
+            yield self.samples[i] if self.presample else self._sample_pair()
 
-        if batch_size is None:
-            raise ValueError('no batch size given')
-
-        info = dict(sfreq=fullinfo['sfreq'],
-                    s_epoch_size=fullinfo['s_epoch_size'],
-                    channels=fullinfo['channels'],
-                    n_samples=n_samples,
-                    batch_size=batch_size,
-                    presample=presample)
-        rng = np.random.RandomState(seed=seed)
-        
-        self.info = info
-        self.rng = rng
+    def _setup(self, seed=1, n_samples=256, batch_size=32, presample=False, **kwargs):
+        self.rng = np.random.RandomState(seed=seed)
+        self.seed = seed
+        self.n_samples = n_samples
+        self.batch_size = batch_size
+        self.presample = presample
 
         if presample:
             self._presample()
 
-    def _sample_recording(self):
-        return self.rng.randint(0, high=len(self.metadata['data']))
  
-    def _sample_pair(self, *args, **kwargs):
-        raise NotImplementedError('no window sampling implemented')
-   
-    def _presample(self):
-        self.sampled = list(self._sample_pair() for _ in range(self.info['n_samples']))
+    def _parameters(self, *args, **kwargs):
+        raise NotImplementedError(
+                'Please implement setup for parameters of pretext-task sampling!')
 
+    def _presample(self):
+        self.samples = list(self._sample_pair() for _ in range(self.n_samples))
+ 
+    def _sample_recording(self):
+        return self.rng.randint(0, high=self.info['n_recordings'])
+    
+    def _sample_window(self, *args, **kwargs):
+        raise NotImplementedError(
+                'Please implement window sampling!')
+   
+    def _sample_pair(self, *args, **kwargs):
+        raise NotImplementedError(
+                'Please implement window-pair sampling!')
+  
