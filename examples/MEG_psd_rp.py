21c07b1bf24ea571c81268643ff230acc0dfe210
ulysses@bund-subsystem <wagren@kth.se>
2021-12-02 11:32:50 +0100

    move example

--- examples/MEG_psd_rp.py
+++ examples/MEG_psd_rp.py
@@ -3,11 +3,11 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-from neurocode.utils import BCEWithLogitsAccuracy
+from neurocode.utils import BCEWithLogitsAccuracy, recording_train_valid_split
 from neurocode.datasets import RecordingDataset, SLEMEG
 from neurocode.samplers import RecordingRelativePositioningSampler
 from neurocode.models import ContrastiveNet, StagerNet
-from braindecode.datautil.preprocess import preprocess, Preprocessor
+from braindecode.datautil.preprocess import preprocess, Preprocessor, zscore
 from braindecode.datautil.windowers import create_fixed_length_windows
 
 
@@ -17,14 +17,16 @@ n_jobs=1
 window_size_s = 5
 sfreq = 200
 window_size_samples = window_size_s * sfreq
-subjects = list(range(2,4))
+subjects = list(range(2,12))
 recordings = [1,3]
 tau_pos = 10
 tau_neg = 30
-n_samples = 256
+gamma = .7
+n_samples = 100
 batch_size = 32
 n_channels = 2
 emb_size = 100
+split = .7
 
 # fetch the dataset
 dataset = SLEMEG(subjects=subjects, recordings=recordings, preload=True, load_meg_only=True)
@@ -37,17 +39,25 @@ preprocessor = [
 preprocess(dataset, preprocessor)
 
 # create 5 second windows of data from the preprocessed SLEMEG dataset
-windows_dataset = create_fixed_length_windows(dataset, start_offset_samples=0, 
-        stop_offset_samples=0, drop_last_window=True, window_size_samples=window_size_samples,
+windows_dataset = create_fixed_length_windows(dataset, start_offset_samples=1, 
+        stop_offset_samples=None, drop_last_window=True, window_size_samples=window_size_samples,
         window_stride_samples=window_size_samples, preload=True)
 
-# reformat from BaseConcatDataset to RecordingDataset
-recording_dataset = RecordingDataset(windows_dataset.datasets, sfreq=sfreq, channels='MEG')
+# normalize all windows channel-wise using zscoring
+preprocess(windows_dataset, [Preprocessor(zscore)])
+
+# split full dataset into train/valid, and reformat to RecordingDatasets
+print(f'splitting dataset into train/valid {int(100*split)}/{int(100 - 100*split)}')
+recordings_train, recordings_valid = recording_train_valid_split(windows_dataset.datasets, split=split)
+datasets = dict(train=RecordingDataset(recordings_train, sfreq=sfreq, channels='MEG'),
+                valid=RecordingDataset(recordings_valid, sfreq=sfreq, channels='MEG'))
 
 # set up relative-positioning sampler with MEG recording dataset
-sampler = RecordingRelativePositioningSampler(recording_dataset.get_data(),
-        recording_dataset.get_info(), tau=tau_pos, gamma=.6,
-        n_samples=n_samples, batch_size=batch_size)
+samplers = dict(train=RecordingRelativePositioningSampler(datasets['train'].get_data(), datasets['train'].get_info(),
+                tau=tau_pos, gamma=gamma, n_samples=n_samples, batch_size=batch_size),
+                valid=RecordingRelativePositioningSampler(datasets['valid'].get_data(), datasets['valid'].get_info(),
+                tau=tau_pos, gamma=gamma, n_samples=n_samples, batch_size=batch_size))
+
 
 # Setup pytorch training, move models etc.
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -62,11 +72,12 @@ emb.train()
 model.train()
 
 print(f'starting model training for {epochs} epochs on device={device}')
-print(f'  epoch      training loss      training acc')
-print(f'--------------------------------------------------')
+print(f'  epoch    training loss   validation loss   training acc   validation acc')
+print(f'----------------------------------------------------------------------------')
 for epoch in range(epochs):
     tloss, tacc = 0., 0.
-    for batch, (anchors, samples, labels) in enumerate(sampler):
+    vloss, vacc = 0., 0.
+    for batch, (anchors, samples, labels) in enumerate(samplers['train']):
         anchors, samples, labels = anchors.to(device), samples.to(device), torch.unsqueeze(labels.to(device), dim=1)
         optimizer.zero_grad()
         outputs = model((anchors, samples))
@@ -75,11 +86,24 @@ for epoch in range(epochs):
         optimizer.step()
         tloss += loss.item() / outputs.shape[0]
         tacc += BCEWithLogitsAccuracy(outputs, labels) / outputs.shape[0]
-    tloss /= len(sampler)
-    tacc /= len(sampler)
+    emb.eval()
+    model.eval()
+    with torch.no_grad():
+        for batch, (anchors, samples, labels) in enumerate(samplers['valid']):    
+            anchors, samples, labels = anchors.to(device), samples.to(device), torch.unsqueeze(labels.to(device), dim=1)
+            outputs = model((anchors, samples))
+            loss = criterion(outputs, labels)
+            vloss += loss.item() / outputs.shape[0]
+            vacc += BCEWithLogitsAccuracy(outputs, labels) / outputs.shape[0]
+    tloss /= len(samplers['train'])
+    vloss /= len(samplers['valid'])
+    tacc /= len(samplers['train'])
+    vacc /= len(samplers['valid'])
     history['tloss'].append(tloss)
+    history['vloss'].append(vloss)
     history['tacc'].append(tacc)
-    print(f'    {epoch+1:02d}          {tloss:.5f}           {100*tacc:.2f}%')
+    history['vacc'].append(vacc)
+    print(f'   {epoch+1:02d}       {tloss:.5f}       {vloss:.5f}       {100*tacc:.2f}%      {100*vacc:.2f}%')
 
 fig, ax1 = plt.subplots(figsize=(8,3))
 ax2 = ax1.twinx()
