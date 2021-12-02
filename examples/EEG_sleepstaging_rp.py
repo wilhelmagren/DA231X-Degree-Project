1fbbbf0ba5fca1e474b5fd5e701fca68f1ca43d3
ulysses@bund-subsystem <wagren@kth.se>
2021-12-02 17:35:27 +0100

    some xample changes to include validation dataset, and plot changes

--- examples/EEG_sleepstaging_rp.py
+++ examples/EEG_sleepstaging_rp.py
@@ -4,7 +4,7 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-from neurocode.utils import BCEWithLogitsAccuracy
+from neurocode.utils import BCEWithLogitsAccuracy, recording_train_valid_split
 from neurocode.datasets import RecordingDataset
 from neurocode.samplers import RelativePositioningSampler
 from neurocode.models import ContrastiveNet, StagerNet
@@ -21,14 +21,15 @@ window_size_s = 30
 sfreq = 100
 window_size_samples = window_size_s * sfreq
 picks = 'eeg'
-subjects = list(range(3))
+subjects = list(range(11))
 recordings = [1]
 tau_pos =  2  # 2*30 seconds = 60 seconds = 1 minute makes up the positive context
 tau_neg = 30  # 30*30 seconds = 900 seconds = 15 minutes makes up the negative context
-n_samples = 256
+n_samples = 100
 batch_size = 32
 n_channels = 2
 emb_size = 100
+split = .7
 
 # fetch the dataset
 dataset = SleepPhysionet(subject_ids=subjects, recording_ids=recordings, crop_wake_mins=30, load_eeg_only=True)
@@ -47,12 +48,14 @@ windows_dataset = create_fixed_length_windows(dataset,
         preload=True, picks=picks)
 
 # Reformat the BaseConcatDataset created by braindecode to a recording-window relative dataset
-recording_dataset = RecordingDataset(windows_dataset.datasets, sfreq=sfreq, channels='eeg')
+recordings_train, recordings_valid = recording_train_valid_split(windows_dataset.datasets, split=split)
+datasets = dict(train=RecordingDataset(recordings_train, sfreq=sfreq, channels='MEG'),
+                valid=RecordingDataset(recordings_valid, sfreq=sfreq, channels='MEG'))
+#recording_dataset = RecordingDataset(windows_dataset.datasets, sfreq=sfreq, channels='eeg')
 
 # Set up a Relative Positioning sampler to use for training the Contrastive Network
-sampler = RelativePositioningSampler(recording_dataset.get_data(), 
-        recording_dataset.get_info(), tau_neg=tau_neg, tau_pos=tau_pos,
-        n_samples=n_samples, batch_size=batch_size)
+samplers = dict(train=RelativePositioningSampler(datasets['train'].get_data(), datasets['train'].get_info(), tau_neg=tau_neg, tau_pos=tau_pos, n_samples=n_samples, batch_size=batch_size),
+                valid=RelativePositioningSampler(datasets['valid'].get_data(), datasets['valid'].get_info(), tau_neg=tau_neg, tau_pos=tau_pos, n_samples=n_samples, batch_size=batch_size, presample=True))
 
 # Setup pytorch training, move models etc.
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -62,7 +65,7 @@ optimizer = torch.optim.Adam(model.parameters(), lr=5e-4, weight_decay=1e-6)
 criterion = torch.nn.BCEWithLogitsLoss()
 
 epochs = 30
-history = dict(tloss=list(), tacc=list())
+history = dict(tloss=list(), tacc=list(), vloss=list(), vacc=list())
 emb.train()
 model.train()
 
@@ -71,7 +74,8 @@ print(f'  epoch      training loss      training acc')
 print(f'--------------------------------------------------')
 for epoch in range(epochs):
     tloss, tacc = 0., 0.
-    for batch, (anchors, samples, labels) in enumerate(sampler):
+    vloss, vacc = 0., 0.
+    for batch, (anchors, samples, labels) in enumerate(samplers['train']):
         anchors, samples, labels = anchors.to(device), samples.to(device), torch.unsqueeze(labels.to(device), dim=1)
         optimizer.zero_grad()
         outputs = model((anchors, samples))
@@ -80,16 +84,31 @@ for epoch in range(epochs):
         optimizer.step()
         tloss += loss.item() / outputs.shape[0]
         tacc += BCEWithLogitsAccuracy(outputs, labels) / outputs.shape[0]
-    tloss /= len(sampler)
-    tacc /= len(sampler)
+    emb.eval()
+    model.eval()
+    with torch.no_grad():
+        for batch, (anchors, samples, labels) in enumerate(samplers['valid']):    
+            anchors, samples, labels = anchors.to(device), samples.to(device), torch.unsqueeze(labels.to(device), dim=1)
+            outputs = model((anchors, samples))
+            loss = criterion(outputs, labels)
+            vloss += loss.item() / outputs.shape[0]
+            vacc += BCEWithLogitsAccuracy(outputs, labels) / outputs.shape[0]
+    tloss /= len(samplers['train'])
+    vloss /= len(samplers['valid'])
+    tacc /= len(samplers['train'])
+    vacc /= len(samplers['valid'])
     history['tloss'].append(tloss)
+    history['vloss'].append(vloss)
     history['tacc'].append(tacc)
-    print(f'    {epoch+1:02d}          {tloss:.5f}           {100*tacc:.2f}%')
+    history['vacc'].append(vacc)
+    print(f'   {epoch+1:02d}       {tloss:.5f}       {vloss:.5f}       {100*tacc:.2f}%      {100*vacc:.2f}%')
 
 fig, ax1 = plt.subplots(figsize=(8,3))
 ax2 = ax1.twinx()
-ax1.plot(history['tloss'], ls='-', marker='d', ms=4, alpha=.7, color='tab:blue', label='training loss')
-ax2.plot(history['tacc'], ls='-', marker='d', ms=4, alpha=.7, color='tab:orange', label='training acc')
+ax1.plot(history['tloss'], ls='-', marker='d', ms=5, alpha=.7, color='tab:blue', label='training loss')
+ax1.plot(history['vloss'], ls=':', marker='d', ms=5, alpha=.7, color='tab:blue', label='validation loss')
+ax2.plot(history['tacc'], ls='-', marker='d', ms=5, alpha=.7, color='tab:orange', label='training acc')
+ax2.plot(history['vacc'], ls=':', marker='d', ms=5, alpha=.7, color='tab:orange', label='validation acc')
 ax1.tick_params(axis='y', labelcolor='tab:blue')
 ax1.set_ylabel('Loss', color='tab:blue')
 ax2.tick_params(axis='y', labelcolor='tab:orange')
--- examples/MEG_psd_rrp.py
+++ examples/MEG_psd_rrp.py
@@ -107,8 +107,10 @@ for epoch in range(epochs):
 
 fig, ax1 = plt.subplots(figsize=(8,3))
 ax2 = ax1.twinx()
-ax1.plot(history['tloss'], ls='-', marker='d', ms=4, alpha=.7, color='tab:blue', label='training loss')
-ax2.plot(history['tacc'], ls='-', marker='d', ms=4, alpha=.7, color='tab:orange', label='training acc')
+ax1.plot(history['tloss'], ls='-', marker='d', ms=5, alpha=.7, color='tab:blue', label='training loss')
+ax1.plot(history['vloss'], ls=':'. marker='d'. ms=5, alpha=.7, color='tab:blue', label='validation loss')
+ax2.plot(history['tacc'], ls='-', marker='d', ms=5, alpha=.7, color='tab:orange', label='training acc')
+ax2.plot(history['vacc'], ls=':', marker='d', ms=5, alpha=.7, color='tab:orange', label='validation acc')
 ax1.tick_params(axis='y', labelcolor='tab:blue')
 ax1.set_ylabel('Loss', color='tab:blue')
 ax2.tick_params(axis='y', labelcolor='tab:orange')
@@ -120,5 +122,4 @@ lines2, labels2 = ax2.get_legend_handles_labels()
 ax2.legend(lines1+lines2, labels1+labels2)
 plt.tight_layout()
 plt.savefig('sleepstaging_relative-positioning_training.png')
-
 
