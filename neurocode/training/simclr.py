12552496a24811141ec9e8725cca988e468e1c5a
ulysses@bund-subsystem <wagren@kth.se>
2022-02-15 10:09:34 +0100

    documentation for SimCLR training module

--- neurocode/training/simclr.py
+++ neurocode/training/simclr.py
@@ -1,8 +1,9 @@
 """
-SimCLR training module, implementing Info NCE Loss.
+SimCLR training module, implementing Info NCE Loss and model.fit() for 
+training encoder f(), 
 
 Authors: Wilhelm Ågren <wagren@kth.se>
-Last edited: 14-02-2022
+Last edited: 15-02-2022
 """
 import torch
 import torch.nn.functional as F
@@ -15,53 +16,94 @@ torch.manual_seed(0)
 
 
 class SimCLR(object):
+    """Simple framework for Contrastive Learning on visual Representations (SimCLR)
+    object, implementing Info NCE Loss and training+validation procedure. 
+
+    Parameters
+    ----------
+    model: torch.nn.Module
+        The initialized neural network to train, usually encoder f() for SimCLR.
+    device: torch.device | str
+        The device on which to place tensors, should be the same as the model is on.
+    optimizer: torch.optim | None
+        The optimization algorithm to apply gradients on. Defaults to Adam.
+    criterion: torch.nn.loss | pytorch_metric_learning.loss | None
+        The loss function to minimize by utilizing the provided optimizer. For 
+        Info NCE Loss you should use torch.nn.CrossEntropyLoss
+    scheduler: torch.optim | None
+        The pytorch scheduler to use, defaults to CosineAnnealingLR.
+        See documentation at pytorch.org for more information.
+    batch_size: int 
+        The batch size of the sampler, for SimCLR this is expanded multiplied by 
+        n_views, since one datapoint x is augmented that amount of times.
+    epochs: int
+        The number of epochs, full iteration of n_samples, to train for. 
+    temperature: float | int 
+        The temperature constant `tau` to use with NT-Xent Loss, and Info NCE Loss.
+        See literature for more information about its characteristics.
+    n_views: int | None
+        The number of aguemnted views to produce given a datapoint x.
     """
-    """
-    def __init__(self, model, device,
-            optimizer=None,
-            scheduler=None,
-            criterion=None,
-            batch_size=256,
-            epochs=100,
-            temperature=.7,
-            n_views=2,
-            **kwargs):
+    def __init__(self, model, device, optimizer=None, criterion=None, scheduler=None,
+            batch_size=256, epochs=100, temperature=.7, n_views=2, **kwargs):
         self.model = model
         self.device = device
         self.optimizer = optimizer
-        self.scheduler = scheduler
         self.criterion = criterion
+        self.scheduler = scheduler
         self.batch_size = batch_size
         self.epochs = epochs
         self.temperature = temperature
         self.n_views = n_views
         
     def info_nce_loss(self, features):
-        """
+        """implementation of Info NCE Loss, as per defined by Oord et al. 2016,
+        takes in the features, expect them the positive pair to lie one batch
+        size deeper in the tensor, i.e. the features tensor has 
+        dimensionality (2*N, D) where N is batch size and D is the embedding size.
+
+        To use cosine-similarity it is required that the features are normalized, 
+        and non-zero. Then, discard the diagonal from both labels and similarity 
+        matrix. Select and combine the multiple positives, and select negatives
+
+        placing all positive logits first results in all correct index
+        labels ending up at position 0, so the zero matrix and the
+        corresponding logits matrix can now simply be sent to
+        the criterion to calculate loss.
+
+        Parameters
+        ----------
+        features: torch.tensor
+            The extracted features from f(), 2N samples where N is the batch size.
+            Thus, there are 2(N-1) negative samples contra the positive samples.
+        
+        Returns
+        -------
+        logits: torch.tensor
+            The log-similarities of the provided positive and negative samples.
+        labels: torch.tensor
+            The labels for where the positive samples are, i.e. 1/2(N-1) possibility
+            to randomly pick the right one. So larger N, tougher contstraint on 
+            training and more reasonable features.
+
         """
         labels = torch.cat([torch.arange(self.batch_size) for _ in range(self.n_views)], dim=0)
         labels = (labels.unsqueeze(0) == labels.unsqueeze(1)).to(self.device)
 
-        # to use cosine-similarity it is required that the features are normalized, and non-zero
         features = F.normalize(features, dim=1)
         sim_mat = torch.matmul(features, features.T)
 
         assert sim_mat.shape == labels.shape, (
                 'Labels and similarity matrix doesn`t have matching shapes.')
 
-        # discard the diagonal from both labels and similarity matrix
         mask = torch.eye(labels.shape[0], dtype=torch.bool).to(self.device)
         labels = labels[~mask].view(labels.shape[0], -1)
         sim_mat = sim_mat[~mask].view(sim_mat.shape[0], -1)
 
-        # select and combine the multiple positives, and select negatives
         positives = sim_mat[labels.bool()].view(labels.shape[0], -1)
         negatives = sim_mat[~labels.bool()].view(sim_mat.shape[0], -1)
 
-        # placing all positive logits first results in all correct index
-        # labels ending up at position 0, so the zero matrix and the
-        # corresponding logits matrix can now simply be sent to
-        # the criterion to calculate loss.
+
         logits = torch.cat([positives, negatives], dim=1)
         labels = torch.zeros(logits.shape[0], dtype=torch.long).to(self.device)
 
@@ -69,15 +111,34 @@ class SimCLR(object):
         return (logits, labels)
 
     def fit(self, samplers, plot=False):
-        print(f'Training encoder with SimCLR on device={self.device} for {self.epochs} epochs')
-        print(f'   epoch       training loss       validation loss         training acc        validation acc')
-        print(f'------------------------------------------------------------------------------------------------')
+        """fit the SimCLR model to the provided samplers, conditionally plot the positive pair
+        augmentations when running for validity, or just for interest. Majority of 
+        parameters for training are set up a priori, i.e. in optimizer, scheduler, which can
+        be found as attributes in the base class.
+
+        Parameters
+        ----------
+        samplers: dict
+            Dictionary containing keys 'train' and 'valid', for the respective datasets.
+            The values are PretextSamplers that should be iterated over, yielding batches
+            of samples according to the pretext task at hand.
+        
+        Returns
+        -------
+        history: dict
+            Dictionary of training+validation results, keys are 'train' and 'valid' and values 
+            are lists of loss. Should be passed to history_plot function from datautil.visualization
+
+        """
         history =  defaultdict(list)
         for epoch in range(self.epochs):
             self.model.train()
             tloss, tacc = 0., 0.
             vloss, vacc = 0., 0.
             for images in samplers['train']:
+                # broadcast the images to their respective anchors and samples, concatenate them
+                # such that on position N in [0, ..., 2N) the first augmented image has its 
+                # positive sample. Then, the labels can easily be created with arange func.
                 anchors, samples = images
                 images = torch.cat((anchors, samples)).float().to(self.device)
                 embeddings = self.model(images)
@@ -85,12 +146,12 @@ class SimCLR(object):
                 if plot:
                     fig, axs = plt.subplots(1, 2)
                     axs[0].imshow(torch.swapaxes(images[0, :].cpu(), 0, 2).numpy())
-                    axs[1].imshow(torch.swapaxes(images[8, :].cpu(), 0, 2).numpy())
+                    axs[1].imshow(torch.swapaxes(images[anchors.shape[0], :].cpu(), 0, 2).numpy())
                     plt.show()
 
                 indices = torch.arange(0, anchors.size(0), device=anchors.device)
                 labels = torch.cat((indices, indices)).to(self.device)
-
+                
                 loss = self.criterion(embeddings, labels)
 
                 self.optimizer.zero_grad()
